03868384dad23b23923badc64dc10ad385ccb3a9
Jason McBride <jsononrails@gmail.com>
2015-11-16 23:44:03 -0500

    Implementing Game Class

---
+++ src/Engine/Core/Engine_GameLoop.js
@@ -0,0 +1,76 @@
+/******************************************************
+* File: Engine_GameLoop.js
+* Description: Game Loop Class
+* Author: Jason McBride
+* Date: Nov 16th 2015
+* Version: 1.0
+*
+* $History$
+* Version 1.0 - Initial
+*******************************************************/
+"use strict" // Operate in Strict mode
+
+var gEngine = gEngine || { };
+
+// Instance variable
+var kFPS = 60;				// Frames per second
+var kMPF = 1000 / kFPS;		// Milleseconds per frame.
+
+// Variables for timing gameloop.
+var mPreviousTime;
+var mLagTime;
+var mCurrentTIme;
+var mElapsedTime;
+
+// The current loop state (running or should stop) 
+var mIsLoopRunning = false;
+
+// Reference to game logic
+var mGame = null;
+
+// This function assumes it is sub-classed from Game
+var _runLoop = function() {
+	if(mIsLoopRunning) {
+		
+		// Step A: set up for next call to _runLoop and update input!
+		requestAnimationFrame(function() { _runLoop.call(mGame); });
+		
+		// Step B: compute elapsed time since last RunLoop was executed
+		mCurrentTIme = Date.now();
+		mElapsedTime = mCurrentTIme - mPreviousTime;
+		mPreviousTime = mCurrentTIme;
+		mLagTime += mElapsedTime;
+		
+		// Setp C: update the game the appropriate number of times
+		// 		   Update only ever Milliseconds per frame.
+		//		   If lag larger than update frames, update until caught up
+		while((mLagTime >= kMPF) && mIsLoopRunning) {
+			this.update();		// call Game.update();
+			mLagTime -= kMPF;
+		}
+		
+		// Step D: now let's draw
+		this.draw();		// Call Game.draw()
+	}
+};
+
+var start = function(Game) {
+	mGame = Game;
+	
+	// Step A: reset frame time
+	mPreviousTime = Date.now();
+	mLagTime = 0.0;
+	
+	// Step B: remember that loop is now running
+	mIsLoopRunning = true;
+	
+	// Step C: request _runLoop to start when loading is done
+	requestAnimationFrame(function() { _runLoop.call(mGame); });
+}; 
+
+gEngine.GameLoop = (function() {
+	var mPublic = {
+		start: start
+	};
+	return mPublic;
+}());
