34e000cc19cf82e302e348f434351df43dddedc4
Jason McBride <jsononrails@gmail.com>
2015-11-17 17:03:35 -0500

    Updated with GameLoop and dot net proj

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -4,7 +4,7 @@ function Game(htmlCanvasID) {
 	this.mConstantColorShader = null;
 	
 	// variables for the squares
-	this.mWhiteSq = null/
+	this.mWhiteSq = null;
 	this.mRedSq = null;
 	
 	// The Camera to view the scene
@@ -22,13 +22,69 @@ Game.prototype.initialize = function() {
 	this.mCamera = new Camera(
 		vec2.fromValues(20, 60),		// position of the camera
 		20,								// width of the camera
-		[20, 40, 600, 300],				// viewport (orgX, orgY, width, height)
+		[20, 40, 600, 300]				// viewport (orgX, orgY, width, height)
 	);
 	
 	this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);	// set background to dark grey
 	
 	// Step B: create the shader
 	this.mConstantColorShader = new SimpleShader(
-		
+	    "/src/GLSLShaders/SimpleVS.glsl",    // Path to Vertexshader
+        "/src/GLSLShaders/SimpleFS.glsl"     // Path to FragmentShader
 	);
+
+    // Step C: Create the renderable objects:
+	this.mWhiteSq = new Renderable(this.mConstantColorShader);
+	this.mWhiteSq.setColor([1, 1, 1, 1]);
+	this.mRedSq = new Renderable(this.mConstantColorShader);
+	this.mRedSq.setColor([1, 0, 0, 1]);
+
+    // Step D: Initialize the white renderable object: centered, 5x5, rotated
+	this.mWhiteSq.getXform().setPosition(20, 60);
+	this.mWhiteSq.getXform().setRotationInRad(0.2);     // In Radian
+	this.mWhiteSq.getXform().setSize(5, 5);
+
+    // Step E: Initialize the red renderable object: centered 2x2
+	this.mRedSq.getXform().setPosition(20, 60);
+	this.mRedSq.getXform().setSize(2, 2);
+
+    // Step F: Start the game loop running
+	gEngine.GameLoop.start(this);
+};
+
+// The update funciton, updates the application state. Make sure to _NOT_ draw
+// anything from this function!
+Game.prototype.update = function() {
+    // For this very simple gam, lets move the white square and pulse the red
+
+    // Step A: move the white square
+    var whiteXform = this.mWhiteSq.getXform();
+    var deltaX = 0.05;
+    if(whiteXform.getXPos() > 30) // this is right-bound of the window
+        whiteXForm.setPosition(10, 60);
+
+    whiteXform.incXPosBy(deltaX);
+    whiteXform.incRotationByDegree(1);
+
+    // Step B: pulse the red square
+    var redXform = this.mRedSq.getXform();
+    if(redXform.getWidth() > 5)
+        redXform.setSize(2, 2);
+    redXform.incSizeby(0.05);
+};
+
+// This is the draw function, make sure to setup proper drawing environment,
+// and more importantly, make sure to _NOT_ change any state.
+Game.prototype.draw = function() {
+    // Step A: clear the canvas
+    gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
+
+    // Step B: Activate the drawing Camera
+    this.mCamera.setupViewProjection();
+
+    // Step C: Activate the white shader to draw
+    this.mWhiteSq.draw(this.mCamera.getVPMatrix());
+
+    // Step D: Activate the red shader to draw
+    this.mRedSq.draw(this.mCamera.getVPMatrix());
 };
